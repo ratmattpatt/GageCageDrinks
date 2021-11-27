2b6ecaae7778ae978a3afdf27f88e01cd3668e44
Marc Angers <marc.angers99@gmail.com>
2021-11-27 22:28:00 +0000

    chenged IP (sad), added mixer functionality :D

--- activatePumps.py
+++ activatePumps.py
@@ -6,6 +6,7 @@ def setup():
 	gpio.setmode(gpio.BCM)
 	gpio.setwarnings(False)
 
+	# setup pumps:
 	gpio.setup(2, gpio.OUT)
 	gpio.setup(3, gpio.OUT)
 	gpio.setup(4, gpio.OUT)
@@ -15,11 +16,14 @@ def setup():
 	gpio.setup(8, gpio.OUT)
 	gpio.setup(9, gpio.OUT)
 
+	# setup pressure plate:
 	gpio.setup(17, gpio.IN)
+	# setup mixer:
+	gpio.setup(27, gpio.OUT)
 
 def activatePumps(pumpArray):
-	#if (gpio.input(17) == 0):
-	#	return "No cup!"
+	if (gpio.input(17) == 0):
+		return "No cup!"
 	
 	for i in range(8):
 		if pumpArray[i] > 0:
@@ -31,6 +35,11 @@ def activatePumps(pumpArray):
 				gpio.output((i+2), gpio.LOW)
 			pumpArray[i] -= 1
 		time.sleep(SHOT_TIME)
+	
+	# mix the ingredients!
+	gpio.output(27, gpio.HIGH)
+	time.sleep(5)
+	gpio.output(27, gpio.LOW)
 
 	return "Success!"
 
